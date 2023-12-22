2ec84ba158d5f59213e8f810591aa57857305b36
tkdhbw <154213359+tkdhbw@users.noreply.github.com>
2023-12-22 12:40:45 +0100

    Update ProxyDisplay.js

--- components/ProxyDisplay.js
+++ components/ProxyDisplay.js
@@ -8,7 +8,9 @@ const ProxyDisplay = () => {
     const fetchProxies = async () => {
       console.log('fetching proxies');
       try {
-        const response = await fetch('https://tkwebservers.com/ProxyList/proxyList.txt');
+        const response = await fetch('https://tkwebservers.com/ProxyList/proxyList.txt', {
+          mode: 'no-cors' // This disables CORS checks
+        });
         const data = await response.text();
         const allLines = data.trim().split('\n');
   
